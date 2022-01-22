82e65c5cf6be57f355d5f25500ee9e9edf65578c
Aleksei Kharlamov <aleksei.kharlamov@zattoo.com>
2022-01-22 21:12:58 +0100

    Use a standard touch driver

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,13 @@
 #include <Arduino.h>
-
-
 #include <Wire.h>
-
 #include <SPI.h>
 #include <TFT_eSPI.h> 
-#include "FT62XXTouchScreen.h"
-
-TFT_eSPI tft = TFT_eSPI();
-FT62XXTouchScreen touchScreen = FT62XXTouchScreen(TFT_WIDTH, PIN_SDA, PIN_SCL);
-
-
+#include <Adafruit_FT6206.h>
 #include "lvgl.h"
 #include "esp_freertos_hooks.h"
+
+TFT_eSPI tft = TFT_eSPI();
+Adafruit_FT6206 touchScreen = Adafruit_FT6206();
 
 static lv_disp_buf_t disp_buf;
 static lv_color_t buf[LV_HOR_RES_MAX * 10];
@@ -21,6 +16,41 @@ lv_obj_t *btn1;
 lv_obj_t *btn2;
 lv_obj_t *screenMain;
 lv_obj_t *label;
+
+bool my_input_read2(lv_indev_drv_t * drv, lv_indev_data_t*data) {
+#ifdef TOUCH_DEBUG
+  Serial.println("#");
+#endif
+
+  static uint16_t lastx = 0;
+  static uint16_t lasty = 0;
+
+  if (!touchScreen.touched()) {
+     data->state = LV_INDEV_STATE_REL;
+     data->point.x = lastx;
+     data->point.y = lasty;
+     return false;
+  }
+
+  TS_Point touchPos = touchScreen.getPoint();
+  data->state = LV_INDEV_STATE_PR;
+
+#ifdef TOUCH_DEBUG
+  Serial.println("touch at " + String(touchPos.x) + "x" + String(touchPos.y));
+#endif
+  auto xpos = touchPos.y;
+  auto ypos = TFT_WIDTH - touchPos.x;
+#ifdef TOUCH_DEBUG
+  Serial.println("mapped to " + String(xpos) + "x" + String(ypos));
+#endif
+
+  data->point.x = xpos;
+  data->point.y = ypos;
+  lastx = xpos;
+  lasty = ypos;
+
+  return false;
+}
 
 static void event_handler_btn(lv_obj_t * obj, lv_event_t event){
     if(event == LV_EVENT_CLICKED) {
@@ -45,43 +75,27 @@ void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color
     lv_disp_flush_ready(disp);
 }
 
-uint16_t lastx = 0;
-uint16_t lasty = 0;
-
-bool my_input_read2(lv_indev_drv_t * drv, lv_indev_data_t*data) {
-  Serial.println("#");
-  TouchPoint touchPos = touchScreen.read();
-  if (touchPos.touched) {
-    Serial.println(String(touchPos.xPos) + ": " + String(touchPos.yPos));
-    data->state = LV_INDEV_STATE_PR;
-    data->point.x = touchPos.xPos;
-    data->point.y = touchPos.yPos;
-    lastx = touchPos.xPos;
-    lasty = touchPos.yPos;
-  } else {
-    data->state = LV_INDEV_STATE_REL;
-     data->point.x = lastx;
-     data->point.y = lasty;
-     
-  }
-  return false;
-}
 static void lv_tick_task(void)
 {
  lv_tick_inc(portTICK_RATE_MS);
+}
+
+void setupPlatform() {
+  esp_err_t err = esp_register_freertos_tick_hook((esp_freertos_tick_cb_t)lv_tick_task); 
 }
 
 void setup() {
   lv_disp_drv_t disp_drv;
   lv_indev_drv_t indev_drv;
 
-  Serial.begin(115200);
+  Serial.begin(9600);
   lv_init();
-  // Setup tick hook for lv_tick_task
-  esp_err_t err = esp_register_freertos_tick_hook((esp_freertos_tick_cb_t)lv_tick_task); 
+
+  setupPlatform();
 
   // Enable TFT
   tft.begin();
+
   tft.setRotation(1);
 
   // Enable Backlight
@@ -89,16 +103,19 @@ void setup() {
   digitalWrite(TFT_BL,1);
 
   // Start TouchScreen
-  touchScreen.begin();
-
+  // requires custom I2C pinout
+  Wire.begin(TOUCH_SDA, TOUCH_SCL);
+  if (!touchScreen.begin(40)) { 
+    Serial.println("Unable to start touchscreen.");
+  }
 
   // Display Buffer
   lv_disp_buf_init(&disp_buf, buf, NULL, LV_HOR_RES_MAX * 10);
 
   // Init Display
   lv_disp_drv_init(&disp_drv);
-  disp_drv.hor_res = 480;
-  disp_drv.ver_res = 320;
+  disp_drv.hor_res = TFT_HEIGHT;
+  disp_drv.ver_res = TFT_WIDTH;
   disp_drv.flush_cb = my_disp_flush;
   disp_drv.buffer = &disp_buf;
   lv_disp_drv_register(&disp_drv);
@@ -107,7 +124,7 @@ void setup() {
   lv_indev_drv_init(&indev_drv);
   indev_drv.type = LV_INDEV_TYPE_POINTER;
   indev_drv.read_cb = my_input_read2;
-  lv_indev_drv_register(&indev_drv);
+  lv_indev_t *indev_drv_t = lv_indev_drv_register(&indev_drv);
 
   // Screen Object
   screenMain = lv_obj_create(NULL, NULL);
@@ -139,6 +156,7 @@ void setup() {
   lv_obj_t * label2 = lv_label_create(btn2, NULL);
   lv_label_set_text(label2, "Goodbye");
 
+  lv_indev_set_cursor(indev_drv_t, label);
   // Screen load
   lv_scr_load(screenMain);
 }
