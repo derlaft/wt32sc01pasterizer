103851c24808ed87c27915a87c252715794d9e13
Aleksei Kharlamov <aleksei.kharlamov@zattoo.com>
2022-01-23 00:20:48 +0100

    use a dummy example

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,9 @@
 #include <SPI.h>
 #include <TFT_eSPI.h> 
 #include <Adafruit_FT6206.h>
-#include "lvgl.h"
 #include "esp_freertos_hooks.h"
+
+#include "lvgl.h"
 
 // platform-related code
 TFT_eSPI tft = TFT_eSPI();
@@ -101,6 +102,27 @@ void my_print(const char * buf)
 }
 #endif
 
+static void btn_event_cb(lv_event_t * e)
+{
+    lv_event_code_t code = lv_event_get_code(e);
+    lv_obj_t * btn = lv_event_get_target(e);
+    if(code == LV_EVENT_CLICKED) {
+        static uint8_t cnt = 0;
+        cnt++;
+
+        /*Get the first child of the button which is the label and change its text*/
+        lv_obj_t * label = lv_obj_get_child(btn, 0);
+        lv_label_set_text_fmt(label, "Тыкнуто: %d", cnt);
+    }
+}
+
+static lv_obj_t * meter;
+
+static void set_value(void * indic, int32_t v)
+{
+    lv_meter_set_indicator_value(meter, (lv_meter_indicator_t*) indic, v);
+}
+
 void setup()
 {
 
@@ -130,17 +152,93 @@ void setup()
     disp_drv.draw_buf = &draw_buf;
     lv_disp_drv_register( &disp_drv );
 
-    /* Initialize the (dummy) input device driver */
+    /* Initialize the input device driver */
     static lv_indev_drv_t indev_drv;
     lv_indev_drv_init( &indev_drv );
     indev_drv.type = LV_INDEV_TYPE_POINTER;
     indev_drv.read_cb = my_input_read;
     lv_indev_drv_register( &indev_drv );
 
-    /* Create simple label */
-    lv_obj_t *label = lv_label_create( lv_scr_act() );
-    lv_label_set_text( label, LVGL_Arduino.c_str() );
-    lv_obj_align( label, LV_ALIGN_CENTER, 0, 0 );
+    // tab one: temperature arc
+    meter = lv_meter_create(lv_scr_act());
+    lv_obj_set_align(meter, LV_ALIGN_TOP_LEFT);
+    lv_obj_set_pos(meter, 20, 20);
+    lv_obj_set_size(meter, 200, 200);
+
+    /*Add a scale first*/
+    lv_meter_scale_t * scale = lv_meter_add_scale(meter);
+    lv_meter_set_scale_ticks(meter, scale, 41, 2, 10, lv_palette_main(LV_PALETTE_GREY));
+    lv_meter_set_scale_major_ticks(meter, scale, 8, 4, 15, lv_color_black(), 10);
+
+    lv_meter_indicator_t * indic;
+
+    /*Add a blue arc to the start*/
+    indic = lv_meter_add_arc(meter, scale, 3, lv_palette_main(LV_PALETTE_BLUE), 0);
+    lv_meter_set_indicator_start_value(meter, indic, 0);
+    lv_meter_set_indicator_end_value(meter, indic, 20);
+
+    /*Make the tick lines blue at the start of the scale*/
+    indic = lv_meter_add_scale_lines(meter, scale, lv_palette_main(LV_PALETTE_BLUE), lv_palette_main(LV_PALETTE_BLUE), false, 0);
+    lv_meter_set_indicator_start_value(meter, indic, 0);
+    lv_meter_set_indicator_end_value(meter, indic, 20);
+
+    /*Add a red arc to the end*/
+    indic = lv_meter_add_arc(meter, scale, 3, lv_palette_main(LV_PALETTE_RED), 0);
+    lv_meter_set_indicator_start_value(meter, indic, 80);
+    lv_meter_set_indicator_end_value(meter, indic, 100);
+
+    /*Make the tick lines red at the end of the scale*/
+    indic = lv_meter_add_scale_lines(meter, scale, lv_palette_main(LV_PALETTE_RED), lv_palette_main(LV_PALETTE_RED), false, 0);
+    lv_meter_set_indicator_start_value(meter, indic, 80);
+    lv_meter_set_indicator_end_value(meter, indic, 100);
+
+    /*Add a needle line indicator*/
+    indic = lv_meter_add_needle_line(meter, scale, 4, lv_palette_main(LV_PALETTE_GREY), -10);
+
+    /*Create an animation to set the value*/
+    lv_anim_t a;
+    lv_anim_init(&a);
+    lv_anim_set_exec_cb(&a, set_value);
+    lv_anim_set_var(&a, indic);
+    lv_anim_set_values(&a, 0, 100);
+    lv_anim_set_time(&a, 2000);
+    lv_anim_set_repeat_delay(&a, 100);
+    lv_anim_set_playback_time(&a, 500);
+    lv_anim_set_playback_delay(&a, 100);
+    lv_anim_set_repeat_count(&a, LV_ANIM_REPEAT_INFINITE);
+    lv_anim_start(&a);
+
+    lv_obj_t * chart;
+    chart = lv_chart_create(lv_scr_act());
+    lv_obj_set_size(chart, 200, 200);
+    lv_obj_center(chart);
+    lv_obj_set_align(chart, LV_ALIGN_TOP_RIGHT);
+    lv_obj_set_pos(chart, -20, 20);
+    lv_chart_set_type(chart, LV_CHART_TYPE_LINE);   /*Show lines and points too*/
+
+    lv_chart_series_t * ser1 = lv_chart_add_series(chart, lv_palette_main(LV_PALETTE_RED), LV_CHART_AXIS_PRIMARY_Y);
+    lv_chart_set_next_value(chart, ser1, 10);
+    lv_chart_set_next_value(chart, ser1, 10);
+    lv_chart_set_next_value(chart, ser1, 10);
+    lv_chart_set_next_value(chart, ser1, 10);
+    lv_chart_set_next_value(chart, ser1, 10);
+    lv_chart_set_next_value(chart, ser1, 10);
+    lv_chart_set_next_value(chart, ser1, 10);
+    lv_chart_set_next_value(chart, ser1, 30);
+    lv_chart_set_next_value(chart, ser1, 70);
+    lv_chart_set_next_value(chart, ser1, 90);
+
+    lv_chart_refresh(chart); /*Required after direct set*/
+
+    lv_obj_t * btn = lv_btn_create(lv_scr_act());
+    lv_obj_set_size(btn, 120, 50);
+    lv_obj_set_align(btn, LV_ALIGN_BOTTOM_MID);
+    lv_obj_set_pos(btn, 0, -20);
+    lv_obj_add_event_cb(btn, btn_event_cb, LV_EVENT_ALL, NULL); 
+
+    lv_obj_t * label = lv_label_create(btn);          /*Add a label to the button*/
+    lv_label_set_text(label, "Тест");                     /*Set the labels text*/
+    lv_obj_center(label);
 
     /* setup done */
     Serial.println( "Setup done" );
@@ -149,5 +247,5 @@ void setup()
 void loop()
 {
     lv_timer_handler(); /* let the GUI do its work */
-    delay( 5 );
+    delay(1);
 }
