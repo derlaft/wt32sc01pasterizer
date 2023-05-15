a79b5684bc711475e1f6086af3bda799629c03c2
Alexey Kharlamov <der@2-47.ru>
2023-05-15 19:39:09 +0200

    8048S070: some fixes

--- src/platform/wt32sc01/Config.h
+++ src/platform/wt32sc01/Config.h
@@ -1,17 +1,21 @@
-// конфигурация различных пинов, связанных в основном с дисплеем и тачем
-#define CONFIG_TOUCH_CS -1
+// тач
 #define TOUCH_SDA 18
 #define TOUCH_SCL 19
-#define ST7796_DRIVER 1
+// SPI
+#define SPI_FREQUENCY 40000000
+#define SPI_READ_FREQUENCY 40000000
 #define USE_HSPI_PORT 1
+// экран
+#define CONFIG_TOUCH_CS -1
+#define ST7796_DRIVER 1
 #define TFT_MISO 12
 #define TFT_MOSI 13
 #define TFT_SCLK 14
 #define TFT_CS 15
 #define TFT_DC 21
 #define TFT_RST 22
-#define SPI_FREQUENCY 40000000
-#define SPI_READ_FREQUENCY 40000000
+// подсветка
 #define TFT_BL 23
+// датчик температуры
 #define DS_CHECK_CRC 1
 #define DS_CRC_USE_TABLE 1
