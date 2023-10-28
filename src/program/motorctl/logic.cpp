ddaaefa5cc5e95ca5e8e5bb0542b4ea24a795bb2
Aleksei Kharlamov <dh@devass.club>
2023-10-28 18:14:32 +0200

    modbus init WIP

--- src/program/motorctl/logic.cpp
+++ src/program/motorctl/logic.cpp
@@ -1,12 +1,11 @@
 #include "logic.hpp"
+#include <ModbusRTU.h>
 
-//ModbusRTU mb;
+ModbusRTU mb;
 
 void logic_setup() {
-	/*
 	Serial1.begin(9600, SERIAL_8N1);
 	mb.begin(&Serial1);
 	mb.setBaudrate(9600);
 	mb.master();
-	*/
 }
