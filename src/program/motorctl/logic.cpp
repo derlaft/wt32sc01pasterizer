07d00d990101f59026b6ba83acc06f46d1ef5f88
Aleksei Kharlamov <dh@devass.club>
2023-11-04 18:05:19 +0100

    logic task

--- src/program/motorctl/logic.cpp
+++ src/program/motorctl/logic.cpp
@@ -8,27 +8,40 @@ HardwareSerial sp(1);
 // TODO
 static uint16_t res = 0;
 
-bool cbWrite(Modbus::ResultCode event, uint16_t transactionId, void* data) {
-  //Serial.printf_P("Request result: 0x%02X, Mem: %d\n", event, ESP.getFreeHeap());
+SemaphoreHandle_t xLogicSemaphore;
+EventGroupHandle_t xLogicGroup;
+
+bool cb(Modbus::ResultCode event, uint16_t transactionId, void* data) { // Modbus Transaction callback
+  if (event != Modbus::EX_SUCCESS)                  // If transaction got an error
+    Serial.printf("Modbus result: %02X\n", event);  // Display Modbus error code
+  if (event == Modbus::EX_TIMEOUT) {    // If Transaction timeout took place
+    Serial.printf("Modbus timeout\n");
+  }
   return true;
 }
 
 void logic_setup() {
-	Serial.println("watin");
-
 	sp.begin(RS485_BAUD, SERIAL_8N1, RS485_RXD, RS485_TXD);
 
 	mb.begin(&sp, RS485_RTS);
 	mb.setBaudrate(RS485_BAUD);
 	mb.master();
 
-	Serial.println("watout");
+	xTaskCreatePinnedToCore(logic_task, "logic", 4096*2, NULL, tskIDLE_PRIORITY+10, NULL, 1);
+}
+
+void logic_task(void *pvParameter) {
+	while (1) {
+		vTaskDelay(pdMS_TO_TICKS(500));
+		mb.task();
+	}
+	vTaskDelete(NULL);
 }
 
 void logic_debug_send_write(uint16_t reg, uint16_t value) {
-	mb.writeHreg(CTL_ADDR, reg, value);
+	mb.writeHreg(CTL_ADDR, reg, value, cb);
 }
 
 void logic_debug_send_read(uint16_t reg) {
-	mb.readHreg(CTL_ADDR, reg, &res, 1, cbWrite);
+	mb.readHreg(CTL_ADDR, reg, &res, 1, cb);
 }
--- src/program/motorctl/logic.hpp
+++ src/program/motorctl/logic.hpp
@@ -6,5 +6,6 @@
 void logic_setup();
 void logic_debug_send_write(uint16_t reg, uint16_t value);
 void logic_debug_send_read(uint16_t reg);
+void logic_task(void *pvParameter);
 
 #endif
