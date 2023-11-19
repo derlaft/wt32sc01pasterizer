7e4876b055d973ee069e362166dd0c833e435e37
Aleksei Kharlamov <dh@devass.club>
2023-11-19 21:35:33 +0100

    old code cleanup

--- src/settings.hpp
+++ src/settings.hpp
@@ -1,28 +1,4 @@
 #ifndef _PAST_SETTINGS_H
 #define _PAST_SETTINGS_H
 
-#include <Arduino.h>
-#include <Preferences.h>
-#include "Config.h"
-#include "ui_events.hpp"
-
-extern int16_t DRAM_ATTR cool_temp_value;
-extern int16_t DRAM_ATTR before_mix_value;
-extern int16_t DRAM_ATTR mix_value;
-extern int16_t DRAM_ATTR mix_delay_value;
-
-extern void IRAM_ATTR settings_setup();
-extern void IRAM_ATTR settings_update();
-
-extern char DRAM_ATTR wifi_ap[128];
-extern char DRAM_ATTR wifi_pw[128];
-
-#define _COOL_TEMP_KEY "cool_temp"
-#define _BEFORE_MIX_KEY "before_mix"
-#define _MIX_KEY "mix"
-#define _MIX_DELAY_KEY "mix_delay"
-
-#define _WIFI_AP_KEY "wssid"
-#define _WIFI_PW_KEY "wpwd"
-
 #endif
--- src/wifi.hpp
+++ src/wifi.hpp
@@ -1,16 +1,5 @@
 #ifndef _WIFI_H
 #define _WIFI_H
 
-#include <Arduino.h>
-#include "settings.hpp"
-#include "Config.h"
-#include "ui_events.hpp"
-#include "ui_hal.h"
-
-#include <WiFi.h>
-#include <WiFiClient.h>
-
-extern void wifi_setup();
-extern void wifi_task(void *pvParameter);
 
 #endif
